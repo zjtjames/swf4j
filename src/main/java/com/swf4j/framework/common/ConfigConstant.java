a8e4122ae57fa2f35da205e648f5f34432f889e3
uzig305 <zjt_james@163.com>
2019-04-17 22:35:11 +0800

    读取配置文件工具类

---
+++ src/main/java/com/swf4j/framework/common/ConfigConstant.java
@@ -0,0 +1,20 @@
+package com.swf4j.framework.common;
+
+/**
+ * 常量，维护配置文件中相关的配置项名称
+ */
+public interface ConfigConstant {
+
+    String CONFIG_FILE = "swf4j.properties";
+
+    String JDBC_DRIVER = "swf4j.framework.jdbc.driver";
+    String JDBC_URL = "swf4j.framework.jdbc.url";
+    String JDBC_USERNAME = "swf4j.framework.jdbc.username";
+    String JDBC_PASSWORD = "swf4j.framework.jdbc.password";
+
+    String APP_BASE_PACKAGE = "swf4j.framework.app.base_package";
+    String APP_JSP_PATH = "swf4j.framework.app.jsp_path";
+    String APP_ASSET_PATH = "swf4j.framework.app.asset_path";
+
+
+}
---
+++ src/main/java/com/swf4j/framework/util/PropsUtil.java
@@ -0,0 +1,59 @@
+/**
+ * created by Zheng Jiateng on 2019/4/17.
+ */
+package com.swf4j.framework.util;
+
+import com.swf4j.framework.common.ConfigConstant;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Properties;
+
+/**
+ * 读取配置文件工具类
+ * 因为是工具类，所以实例变量和方法全是静态的
+ */
+public class PropsUtil {
+
+    private static Logger logger = LoggerFactory.getLogger(PropsUtil.class);
+
+    private static Properties props;
+
+    static{
+        String fileName = ConfigConstant.CONFIG_FILE;
+        props = new Properties();
+        InputStreamReader inputStreamReader = null;
+        try {
+            inputStreamReader = new InputStreamReader(PropsUtil.class.getClassLoader().getResourceAsStream(fileName), "UTF-8");
+            props.load(inputStreamReader);
+        } catch (IOException e) {
+            logger.error("配置文件读取异常", e);
+        }
+    }
+
+    /**
+     * 获取属性（默认值为null）
+     */
+    private static String getProperty(String key) {
+        String value = props.getProperty(key.trim());
+        if (StringUtils.isBlank(value)) {
+            return null;
+        }
+        return value.trim();
+    }
+
+    /**
+     * 获取属性（可指定默认值）
+     */
+    private static String getProperty(String key, String defaultValue) {
+        String value = props.getProperty(key.trim());
+        if (StringUtils.isBlank(value)) {
+            return defaultValue;
+        }
+        return value.trim();
+    }
+
+}
