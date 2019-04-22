4423db4baea6def6668d6af2e7d24b6f4a1574c2
uzig305 <zjt_james@163.com>
2019-04-22 10:38:22 +0800

    doc

--- src/main/java/com/swf4j/framework/common/ConfigConstant.java
+++ src/main/java/com/swf4j/framework/common/ConfigConstant.java
@@ -12,6 +12,7 @@ public interface ConfigConstant {
     String JDBC_USERNAME = "swf4j.framework.jdbc.username";
     String JDBC_PASSWORD = "swf4j.framework.jdbc.password";
 
+    // 整个应用的基础包名
     String APP_BASE_PACKAGE = "swf4j.framework.app.base_package";
     String APP_JSP_PATH = "swf4j.framework.app.jsp_path";
     String APP_ASSET_PATH = "swf4j.framework.app.asset_path";
