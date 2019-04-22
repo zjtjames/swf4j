fae42b09f0dd80958ecf4e81630ba4efffc22bbd
uzig305 <zjt_james@163.com>
2019-04-22 15:37:21 +0800

    反射工具类：通过类来实例化对象，调用方法，设置成员变量的值

---
+++ src/main/java/com/swf4j/framework/util/ReflectionUtil.java
@@ -0,0 +1,60 @@
+/**
+ * created by Zheng Jiateng on 2019/4/22.
+ */
+package com.swf4j.framework.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+/**
+ * 反射工具类：通过类来实例化对象，调用方法，设置成员变量的值
+ */
+public final class ReflectionUtil {
+
+    private static Logger logger = LoggerFactory.getLogger(ReflectionUtil.class);
+
+    /**
+     * 创建实例
+     */
+    public static Object newInstance(Class<?> cls) {
+        Object instance;
+        try {
+            instance = cls.newInstance();
+        } catch (Exception e) {
+            logger.error("create new instance failure", e);
+            throw new RuntimeException(e);
+        }
+        return instance;
+    }
+
+    /**
+     * 调用方法
+     */
+    public static Object invokeMethod(Object obj, Method method, Object... args) {
+        Object result;
+        try {
+            method.setAccessible(true);
+            result = method.invoke(obj, args);
+        } catch (Exception e) {
+            logger.error("invoke method failure", e);
+            throw new RuntimeException(e);
+        }
+        return result;
+    }
+
+    /**
+     * 设置成员变量的值
+     */
+    public static void setField(Object obj, Field field, Object value) {
+        try {
+            field.setAccessible(true);
+            field.set(obj, value);
+        } catch (Exception e) {
+            logger.error("set field failure", e);
+            throw new RuntimeException(e);
+        }
+    }
+}
