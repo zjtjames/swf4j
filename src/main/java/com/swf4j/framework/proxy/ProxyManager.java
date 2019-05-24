f7da975b0fcf551399cd11cd31af1388b683cf6a
uzig305 <zjt_james@163.com>
2019-05-24 20:55:02 +0800

    泛型方法 Class<> 中应该是T 而不是通配符

--- src/main/java/com/swf4j/framework/proxy/ProxyManager.java
+++ src/main/java/com/swf4j/framework/proxy/ProxyManager.java
@@ -11,15 +11,12 @@ import java.lang.reflect.Method;
 import java.util.List;
 
 /**
- * 代理管理器，只有一个方法，用来创建代理对象
+ * 代理管理器，提供一个创建代理对象的方法，输入一个目标类和一组Proxy接口实现，输出一个代理对象
  */
 public class ProxyManager {
 
-    /**
-     * 创建代理对象：输入一个目标类和一组Proxy接口实现，输出一个代理对象
-     */
     @SuppressWarnings("unchecked") // 使用unchecked忽略编译时的向下转型警告
-    public static <T> T createProxy(Class<?> targetClass, List<Proxy> proxyList) {
+    public static <T> T createProxy(Class<T> targetClass, List<Proxy> proxyList) {
         return (T) Enhancer.create(targetClass, new MethodInterceptor() {
             @Override
             public Object intercept(Object targetObject, Method targetMethod, Object[] methodParams, MethodProxy methodProxy) throws Throwable {
